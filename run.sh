d44379c887e17e6bde0c47193ad2f9c6de16a3cf
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2024-11-19 01:29:04 +0000

    init

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+helm repo add mattermost https://helm.mattermost.com || true
+
+helm upgrade \
+    --install mattermost \
+    mattermost/mattermost-team-edition \
+    --namespace mattermost \
+    --create-namespace \
+    --set mysql.mysqlUser=sampleUser \
+    --set mysql.mysqlPassword=samplePassword \
+    --set ingress.enabled=true
