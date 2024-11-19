d37b83f48da8797c1b24c3be6855a9cdebd630d6
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2024-11-19 01:41:21 +0000

    save

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+# This script sets up and installs Mattermost using Minikube and Helm, 
+# and then forwards the necessary port for access.
+
+# Starts Minikube.
 minikube start
 
+# Adds the Mattermost Helm repository.
 helm repo add mattermost https://helm.mattermost.com
 
+# Installs or upgrades Mattermost Team Edition using Helm.
 helm upgrade \
     --install mattermost \
     mattermost/mattermost-team-edition \
@@ -12,3 +18,9 @@ helm upgrade \
     --set mysql.mysqlUser=sampleUser \
     --set mysql.mysqlPassword=samplePassword \
     --set ingress.enabled=true
+
+# Set context to the Mattermost namespace.
+kubectl config set-context --current --namespace=mattermost
+
+# Forwards port 8065 to access Mattermost.
+kubectl port-forward svc/mattermost-team-edition 8065:8065 --namespace mattermost
